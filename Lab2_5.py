6a3aeddab1031e4c580111dd983ab7fa6b5d38ba
Игорь <testmail@testmail.com>
2025-09-22 05:02:28 +0500

    Выполнены лабораторные и самостоятельые работы

---
+++ Lab2_5.py
@@ -0,0 +1,7 @@
+one = input("one:")
+two = input("two:")
+three = input("three:")
+
+print(one, two, three)
+
+
---
+++ SW2_5.py
@@ -0,0 +1,2 @@
+day, month, year  = 22,"Сентября", 2025
+print(f"Сегодня {day} {month} {year} ",end="Всего хорошего!")
